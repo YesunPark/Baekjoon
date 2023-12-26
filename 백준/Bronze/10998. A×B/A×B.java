f40aaef6e34815295101c673052350597bdd3969
yesun <108933466+YesunPark@users.noreply.github.com>
2023-12-26 22:33:46 +0900

    [Bronze V] Title: A×B, Time: 132 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String[] strArr = str.split(" ");
+
+        System.out.println(Integer.parseInt(strArr[0]) * Integer.parseInt(strArr[1]));
+    }
+}
