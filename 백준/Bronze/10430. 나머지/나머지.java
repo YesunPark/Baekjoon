97f28b89be0208074b10aade42c0f7fc03a18229
yesun <108933466+YesunPark@users.noreply.github.com>
2023-12-31 21:39:11 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String[] strs = str.split(" ");
+        int A = Integer.parseInt(strs[0]);
+        int B = Integer.parseInt(strs[1]);
+        int C = Integer.parseInt(strs[2]);
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+    }
+}
