3081c01c501c9940e09baf3d90a8c0defaaf48fb
yesun <108933466+YesunPark@users.noreply.github.com>
2024-01-08 22:10:28 +0900

    [Bronze V] Title: A+B - 3, Time: 148 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(
+            new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            String str = br.readLine();
+            int A = Integer.parseInt(str.split(" ")[0]);
+            int B = Integer.parseInt(str.split(" ")[1]);
+            bw.write( (A + B) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
